72d2f6d8e386b3f05d11f86c8412814f78665452
Morten <bitbucket@moddi.dk>
2020-12-08 13:52:30 +0100

    Updated delays to be the production values

--- FormVisualization/SmartMirrorController.cs
+++ FormVisualization/SmartMirrorController.cs
@@ -25,11 +25,11 @@ namespace FormVisualization
 
         public event EventHandler IconsUpdated;
 
-        private int handwashDelay = 3000; //15*1000
-        private int handwashStop = 10000; //5*60*1000
-        private int soapNeededDelay = 3000; //5*1000
-        private int soapNeededStop = 10000; //1*60*1000
-        private int handwashDiffTime = 15000; //5*60*1000
+        private int handwashDelay = 1;
+        private int handwashStop = 5 * 60 * 1000;
+        private int soapNeededDelay = 5 * 1000;
+        private int soapNeededStop = 1 * 60 * 1000;
+        private int handwashDiffTime = 5 * 60 * 1000;
 
         //TODO datetime for events
         private DateTime lastHandwashDate;
@@ -39,6 +39,15 @@ namespace FormVisualization
         private void TriggerUpdate()
         {
             IconsUpdated?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void SetTestValues()
+        {
+            handwashDelay = 3 * 1000;
+            handwashStop = 10 * 1000;
+            soapNeededDelay = 3 * 1000;
+            soapNeededStop = 10 * 1000;
+            handwashDiffTime = 15 * 1000;
         }
 
         private Timer CreateActiveTimer(int time, ElapsedEventHandler eventHandler)
@@ -116,6 +125,18 @@ namespace FormVisualization
         public void ToothbrushEvent(BasicEvent evt)
         {
 
+        }
+
+        public void Reset()
+        {
+            toiletTimer?.Close();
+            soapNeededTimer?.Close();
+            soapNeededStopTimer?.Close();
+            handwashStopTimer?.Close();
+
+            lastHandwashDate = DateTime.Now.Subtract(new TimeSpan(24, 0, 0));
+            lastSoapDate = DateTime.Now.Subtract(new TimeSpan(24, 0, 0));
+            lastToothbrushDate = DateTime.Now.Subtract(new TimeSpan(24, 0, 0));
         }
 
     }
